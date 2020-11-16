022d334fdd76a6436364770c94cfed0e821e85f0
Ivan Vuksanov <ivan.vuksanov@guestready.com>
2020-11-16 19:27:11 +0000

    old project backup

--- src/components/header.js
+++ src/components/header.js
@@ -1,31 +1,19 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import './Header.css'
+
 
 const Header = ({ siteTitle }) => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+  <div className="Header">
+    <div className="HeaderContent">
+      <Link to="/"><img src={require('../images/SolarWise_Logo.svg')} width="140px"/></Link>
+      <div className="MenuItems">
+        <Link to="/">About</Link>  
+        <Link to="/">Solar 101</Link>  
+        <Link to="/">Estimator</Link>  
+        <Link to="/">Contact us</Link> 
+      </div>
+      
     </div>
   </div>
 )
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,15 +2,20 @@ import React from 'react'
 import Link from 'gatsby-link'
 
 const IndexPage = () => (
-  <div>
-    <h1>Everything you need for a smooth transition to solar energy.</h1>
-    <h4>SolarWise wil help you:</h4>
-    <ul>
-      <li>Find the most efficient solutions and products mix</li>
-      <li>Save money and reduce your environmental footprint</li>
-    </ul>
-    <Link to="/page-2/">Go to page 2</Link>
-  </div>
+<div>
+	<div className="Hero">
+		<div className="HeroGroup">
+			<h1>Everything you need for a smooth transition to solar energy.</h1>
+			<h4>SolarWise wil help you:</h4>
+			<ul>
+				<li>Find the most efficient solutions and products mix</li>
+				<li>Save money and reduce your environmental footprint</li>
+			</ul>
+			<Link to="/page-2/">Button</Link>
+		</div>
+	</div>
+</div>	
+  
 )
 
 export default IndexPage
